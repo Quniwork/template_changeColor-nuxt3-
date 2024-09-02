d3d371715e66d983d36f6b7b2336373fb5650d0f
Quni <chumini0121@gmail.com>
2024-09-02 17:03:41 +0800

    commit 9.2.2

--- components/ChangeColor.vue
+++ components/ChangeColor.vue
@@ -1,9 +1,18 @@
 <template>
   <div ref="changeColorBlock" class="changeColor-block">
+    <!-- <div class="changeColor-edit">
+      <button class="edit">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor"><path d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152L0 424c0 48.6 39.4 88 88 88l272 0c48.6 0 88-39.4 88-88l0-112c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 112c0 22.1-17.9 40-40 40L88 464c-22.1 0-40-17.9-40-40l0-272c0-22.1 17.9-40 40-40l112 0c13.3 0 24-10.7 24-24s-10.7-24-24-24L88 64z"/></svg>
+      </button>
+    </div> -->
     <div class="changeColor-wrap">
       <div class="changeColor-default">
-        <button class="btn theme-dark"><i class="fa-solid fa-moon"></i></button>
-        <button class="btn theme-light"><i class="fa-solid fa-sun-bright"></i></button>
+        <div class="color-item" @click="setTheme('dark')" :class="{ active: theme === 'dark' }">
+          Dark
+        </div>
+        <div class="color-item" @click="setTheme('light')" :class="{ active: theme === 'light' }">
+          Light
+        </div>
       </div>
       <div
         class="changeColor-item"
@@ -41,9 +50,27 @@
 import { ref } from 'vue';
 import html2canvas from 'html2canvas'; // 导入 html2canvas
 
+const theme = ref('dark')
+const setTheme = (newTheme) => {
+  theme.value = newTheme
+  document.documentElement.setAttribute('data-theme', newTheme)
+}
+onMounted(() => {
+  // 預設設置 data-theme 為 'dark'
+  document.documentElement.setAttribute('data-theme', theme.value)
+})
+
 // 定义一个包含颜色选择器数据的数组
 const colorPickers = ref([
   { label: '更改主色', variable: '--color-primary', value: '#0E77FF' },
+  { label: '文字顏色', variable: '--color-primary-text', value: '#f5f5f5' },
+  { label: '輔助文字顏色', variable: '--color-secondary-text', value: '#888888' },
+  { label: '背景色', variable: '--color-primary-bg', value: '#000000' },
+  { label: '輔助背景色', variable: '--color-secondary-bg', value: '#212224' },
+  { label: '大廳背景色', variable: '--color-platform-bg', value: '#212224' },
+  { label: 'Header背景色', variable: '--color-header-bg', value: '#212224' },
+  { label: 'Footer背景色', variable: '--color-footer-bg', value: '#434343' },
+  { label: '更改登入欄位背景色', variable: '--color-login-input', value: '#434343' },
   { label: '更改登入按鈕背景色', variable: '--color-login-button', value: '#2C88FF' },
   { label: '更改註冊按鈕背景色', variable: '--color-join-button', value: '#0EACFF' },
 ]);
